0c3489ab5dc1c69e13dbc00cec2be3f1a54753ff
Corbin Rose <corbinisnotyou@gmail.com>
2019-06-14 00:06:17 -0400

    updating index

--- index.php
+++ index.php
@@ -9,4 +9,29 @@ if(isset($_REQUEST['era'])){
 }
 $url = "http://www.biblicalcalendarproof.com/find/Json/JSONGetCalendarByYear?year={$year}&era={$era}";
 	echo $url;
+
+	echo "<br />";
+
+$handle = curl_init();
+
+// Set the url
+curl_setopt($handle, CURLOPT_URL, $url);
+// Set the result output to be a string.
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+ 
+$output = curl_exec($handle);
+ 
+curl_close($handle);
+
+//var_dump(json_decode($output, true));
+
+echo json_decode($output, true);
+
+$fp = fopen('output/' .$era . $year . '.json', 'w');
+fwrite($fp, json_encode($output));
+fclose($fp);
+
+$fw = fopen('output/'. $era . $year .'html', 'w');
+fwrite($fw, json_decode($output));
+fclose($fw);
 ?>
