baac3d6a44c74d8debb47539e5fadab31a7e4760
Corbin <corbinisnotyou@gmail.com>
2019-06-15 23:25:22 -0400

    updating loop

--- index.php
+++ index.php
@@ -1,4 +1,34 @@
 <?php 
+
+function getCalendarByYear($year, $era){
+	$url = "http://www.biblicalcalendarproof.com/find/Json/JSONGetCalendarByYear?year={$year}&era={$era}";
+
+	$handle = curl_init();
+
+	// Set the url
+	curl_setopt($handle, CURLOPT_URL, $url);
+	// Set the result output to be a string.
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+	 
+	$output = curl_exec($handle);
+	 
+	curl_close($handle);
+
+	$fp = fopen('output/' .$era . $year . '.json', 'w');
+	fwrite($fp, json_encode($output));
+	fclose($fp);
+
+	$fw = fopen('output/'. $era . $year .'html', 'w');
+	fwrite($fw, json_decode($output));
+	fclose($fw);
+
+
+	echo 'writing output/' .$era . $year . '.json | ';
+	echo 'writing output/'. $era . $year .'html<br />';
+}
+
+
+/*
 $year = "33";
 if(isset($_REQUEST['year'])){
 	$year = $_REQUEST['year'];
@@ -7,31 +37,17 @@ $era = "AD";
 if(isset($_REQUEST['era'])){
 	$era = $_REQUEST['era'];
 }
-$url = "http://www.biblicalcalendarproof.com/find/Json/JSONGetCalendarByYear?year={$year}&era={$era}";
-	echo $url;
+*/
 
-	echo "<br />";
+for($i=-4000;$i<2020;$i++){
+	
+	$era = "BC";
+	if($i>0){
+		$era = "AD";
+	}
+	$year = $i;
+	echo $era . $year . "<br />";
+	//getCalendarByYear($year, $era);
+}
 
-$handle = curl_init();
-
-// Set the url
-curl_setopt($handle, CURLOPT_URL, $url);
-// Set the result output to be a string.
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
- 
-$output = curl_exec($handle);
- 
-curl_close($handle);
-
-//var_dump(json_decode($output, true));
-
-echo json_decode($output, true);
-
-$fp = fopen('output/' .$era . $year . '.json', 'w');
-fwrite($fp, json_encode($output));
-fclose($fp);
-
-$fw = fopen('output/'. $era . $year .'html', 'w');
-fwrite($fw, json_decode($output));
-fclose($fw);
 ?>
