1d48bbdb50fd2bc703d976e1c8e491092d3f8778
Corbin <corbinisnotyou@gmail.com>
2019-06-16 10:29:47 -0400

    making it look nicer

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+	rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<script
   src="https://code.jquery.com/jquery-2.2.4.min.js"
   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
@@ -24,9 +26,39 @@
 
   });
   </script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+  crossorigin="anonymous"></script>
 </head>
 <body>
-	
+<header>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Calendar Downloader</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+       <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Download ALL</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Download  Specific Year</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Results</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</header>
+<br />
 <?php 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -69,7 +101,9 @@ if(isset($_REQUEST['year']) && isset($_REQUEST['era'])) {
 	if(isset($_REQUEST['era'])){
 		$era = $_REQUEST['era'];
 	}
+	echo "<div class='container'><div class='alert alert-success'>";
 	getCalendarByYear($year, $era);
+	echo "</div></div>";
 } else {
 
 	//echo "not single";
@@ -109,11 +143,18 @@ if(isset($_REQUEST['year']) && isset($_REQUEST['era'])) {
 				$thisYear *=-1;
 			}
 			
-			echo $k . " | " . $thisEra . " " . $thisYear . "<br />";
+			//echo $k . " | " . $thisEra . " " . $thisYear . "<br />";
+			echo "<div class='container'><div class='alert alert-success'>";
 			getCalendarByYear($thisYear, $thisEra);
+			echo "</div></div>";
 		}
 	} else {
 
+		?>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+		<?php
 		echo "<h1>Download by sets of 50</h1>";
 		$maxCounter = 0;
 
@@ -166,6 +207,28 @@ if(isset($_REQUEST['year']) && isset($_REQUEST['era'])) {
 			echo "<li><a href='index.php?from={$from}&to={$to}'>". $years ."</a></li>";
 		}
 		echo "</ul>";
+
+		?>
+			</div>
+			<div class="col-sm-6">
+				<h1>Get a specific year</h1>
+				<form action="index.php" method="GET">
+				<label>Year</label>
+				<input type="text" id="year" name="year"  class="form-control"/>
+				<label>Era</label>
+				<select class="form-control" name="era">
+					<option value="AD">AD</option>
+					<option value="BC">BC</option>
+				</select>
+				<br />
+				<button class="btn btn-primary">
+					Download
+				</button>
+				
+			</div>
+		</div>
+
+		<?php
 	}
 
 
