152004611d01ff3ab541556f29d9f5467a66bc59
Corbin Rose <corbinisnotyou@gmail.com>
2019-10-25 07:25:28 -0400

    siggy dates

--- significantDates.php
+++ significantDates.php
@@ -1,4 +1,49 @@
+<html lang="en" dir="ltr" prefix="content: http://purl.org/rss/1.0/modules/content/ dc: http://purl.org/dc/terms/ foaf: http://xmlns.com/foaf/0.1/ og: http://ogp.me/ns# rdfs: http://www.w3.org/2000/01/rdf-schema# sioc: http://rdfs.org/sioc/ns# sioct: http://rdfs.org/sioc/types# skos: http://www.w3.org/2004/02/skos/core# xsd: http://www.w3.org/2001/XMLSchema#">
+<head>
+  <link rel="profile" href="http://www.w3.org/1999/xhtml/vocab" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Generator" content="Drupal 7 (http://drupal.org)" />
+<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.css" media="all" />
+<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@unicorn-fail/drupal-bootstrap-styles@0.0.2/dist/3.3.1/7.x-3.x/drupal-bootstrap.css" media="all" />
+<style>
+.badge {margin: 4px;}
+</style>
+
+
+<style>table, td {border: 1px solid #ddd; padding: 5px;}</style>
+</head>
+
+
+<body>
+	<script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+<script>
+$(document).ready(function(){
+
+
+	$("table a").click(function(e){
+		e.preventDefault();
+		var linky = $(this).attr("href");
+		$.ajax({
+			url: linky,
+			success: function(data){
+				var thisLink = $(data).find(".sdHeadline a").attr("href");
+				thisLink = "https://www.biblicalcalendarproof.com" + thisLink;
+				window.open(thisLink)
+			}, 
+			error: function(e){
+
+			}
+		});
+	});
+});
+</script>
 <?php
+$env = "local";
+include "../generator/calendar/conn.php";
 $dates = array(
 "BC4046",
 "BC3915",
@@ -144,8 +189,29 @@ $dates = array(
 "BC4",
 "AD30",
 "AD70");
+echo "<table><tr><th>Era</th><th>Year</th><th>AM</th></tr>";
+foreach($dates as $date){
+	//echo $date;
+	$era = substr($date, 0,2);
+	$year  = substr($date, 2);
+	echo  "<tr><td>" . $era . " </td><td> " . $year . "</td><td>";
+	$where ="GC_Era = '" . $era . "' and GC_Year = '" . $year . "'";
+	//echo $where . "<br />";
+	$sql = "SELECT AM FROM calendardates where " . $where;
+	//echo $sql . "<hr />";
+	$result = $conn->query($sql);
+	while($row = $result->fetch_assoc()) {
+		echo "<a href='../generator/calendar/download/" . $date . ".html'>";
+		echo $row['AM'];
+		echo "</a>";
+	}
+	echo "</td></tr>";
+	//print_r($result);
+}
 
-echo "<pre>";
-print_r($dates);
-echo "</pre>";
+//echo "<pre>";
+//print_r($dates);
+//echo "</pre>";
 ?>
+</body>
+</html>
